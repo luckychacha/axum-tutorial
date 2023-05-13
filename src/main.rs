645b8a32facf521dba92945ee42eca2f66efcf4d
luckychacha <358140284@qq.com>
2023-05-13 23:53:01 +0800

    Axum Hello wrold.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World!" }));
+    let host = "127.0.0.1";
+    let port = "3000";
+    let builder = axum::Server::bind(&format!("{host}:{port}").parse().unwrap());
+    println!("Server start at: {host}:{port}");
+
+    builder.serve(app.into_make_service()).await.unwrap();
 }
