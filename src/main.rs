10d1430cefa436daea1e9c51fd2460277cd6a1e1
luckychacha <358140284@qq.com>
2023-05-15 00:45:54 +0800

    impl 2 router simply.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::{get, post},
+    Extension, Json, Router, Server,
+};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(|| async { "Hello World!" }))
-        .route("/foo", get(get_foo).post(post_foo));
+        .route("/tickets", post(post_foo))
+        .route("/foo/:id", get(get_foo));
     let host = "127.0.0.1";
     let port = "3000";
     let builder = axum::Server::bind(&format!("{host}:{port}").parse().unwrap());
@@ -13,5 +21,20 @@ async fn main() {
     builder.serve(app.into_make_service()).await.unwrap();
 }
 
-async fn get_foo() {}
-async fn post_foo() {}
+async fn get_foo(Path(id): Path<u64>) {
+    println!("{id:?}");
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Foo {
+    pub uid: u64,
+    pub uname: String,
+}
+async fn post_foo(Json(foo): Json<Foo>) -> impl IntoResponse {
+    println!("{foo:?}");
+    println!("{:?}", foo.uname);
+    // test(foo.clone()).await?;
+    Json(foo)
+}
+
+async fn test(foo: Foo) {}
